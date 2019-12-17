8a8811d90c2b452b8e726adee3ebefdcd31a3b5c
impuneet <iampuneet.in@gmail.com>
2019-12-17 13:04:26 +0530

    saving

--- src/components/Footer/Footnote.js
+++ src/components/Footer/Footnote.js
@@ -29,7 +29,9 @@ const styles = theme => ({
 });
 
 const Footnote = ({ classes, content }) => {
-  return <div className={classes.container} dangerouslySetInnerHTML={{ __html: content }} />;
+  return <div className={classes.container} dangerouslySetInnerHTML={{ __html: content }}>
+    <a href="https://www.a2hosting.com?aid=kajalsharma6123&amp;bid=ed1c4a67" target="_top"><img src="//affiliates.a2hosting.com/accounts/default1/banners/ed1c4a67.jpg" alt="" title="" width="728" height="90" /></a><img style="border:0" src="https://affiliates.a2hosting.com/scripts/imp.php?aid=kajalsharma6123&amp;bid=ed1c4a67" width="1" height="1" alt="" />
+  </div>;
 };
 
 Footnote.propTypes = {
